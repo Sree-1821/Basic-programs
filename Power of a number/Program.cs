95120243734333de5586944343a4a25993dbad2d
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-04-28 19:37:59 +0530

    Add files via upload

---
+++ Power of a number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace power_of_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a base  number");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a expo number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            int result = 1;
+            for (int i = 1; i <= x; i++)
+            {
+                result = result * n;
+            }
+
+                Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+    }
+    }
+    
+
