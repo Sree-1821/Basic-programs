14a537e0e9d46798c62da0fab7a4016f20b95e19
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-04-29 19:27:31 +0530

    Add files via upload

---
+++ string to int/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace string_to_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string  str = "100";
+            int n = Convert.ToInt32(str);
+            Console.WriteLine(n);
+            Console.ReadLine();
+        }
+      
+    }
+}
