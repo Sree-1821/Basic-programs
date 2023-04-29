9c7372d9a8b1dfc526b903a25d113e40c9e74c36
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-04-29 14:12:27 +0530

    Add files via upload

---
+++ Repeated char/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reprated_charchters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string str = Console.ReadLine();
+            while(str.Length>0)
+            {
+                int countofchar = 0;
+                Console.WriteLine(str[0]);
+                for(int i=0;i<str.Length;i++)
+                {
+                    if(str[0]==str[i])
+                    {
+                        countofchar++;
+
+                    }
+                }
+                Console.WriteLine(countofchar);
+                str = str.Replace(str[0].ToString(), string.Empty);
+
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
