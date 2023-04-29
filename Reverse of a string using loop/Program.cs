b076956099cabdf78dc14753a5d82e9f5d057a45
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-04-29 14:09:45 +0530

    Add files via upload

---
+++ Reverse of a string using loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_string_using_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string str = Console.ReadLine();
+            for(int i=str.Length;i>0;i--)  // sree=lenth =4 ,4>0
+            {
+                Console.Write(str[i-1]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
