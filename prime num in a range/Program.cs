ef4cf1d81ac61a9b7134b9345beb0262b4ef4b8e
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-05-01 10:59:57 +0530

    Add files via upload

---
+++ prime num in a range/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_no_in_range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int count;
+            Console.WriteLine("enter a staring number");
+            int startnum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter a ending number");
+            int endingnum = Convert.ToInt32(Console.ReadLine());
+             for(int i=startnum;i<=endingnum;i++)
+            {
+                count = 0;
+                for( int j=1;j<=i; j++)
+                {
+                    if (i % j == 0)
+                        count++;
+                }
+                if(count==2)
+                {
+                    Console.WriteLine("given number are prime number: " + i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
+        
