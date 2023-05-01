e85f77ce40b029ab994a772b59c3da4471e3f4eb
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-05-01 13:16:03 +0530

    Add files via upload

---
+++ avg program/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace avg_of_a_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            double avg;
+            Console.WriteLine("enter a  10 number");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            for(int i=11;i<=20;i++)
+            {
+                sum = sum + i;
+                Console.WriteLine("sum is :" +sum);
+                
+            }
+            avg = sum / 10;
+            Console.WriteLine(" avg is :" +avg);
+            Console.ReadLine();
+            
+        }
+    }
+}
