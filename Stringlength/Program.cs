73426790280f55fc3264a8c380f6ae8060b3feaa
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-05-01 11:08:18 +0530

    Add files via upload

---
+++ Stringlength/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringlength
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count =0;
+            Console.WriteLine("enter a string");
+            string str=  Console.ReadLine();
+            for(int i=0; str[i]!=0;i++)
+            {
+                count++;
+            }
+            
+                 
+            
+            
+                Console.WriteLine( " Length is :" + count);
+
+            
+            Console.ReadLine();
+        }
+
+    }
+}
