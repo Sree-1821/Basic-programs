f088978e69e371f070c35443a40cd601c0b7c6a7
Sree-1821 <115399036+Sree-1821@users.noreply.github.com>
2023-05-01 11:05:12 +0530

    Add files via upload

---
+++ Count of digit/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COUNT_OF_DIGITS_IN_INTEGER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int reminder, count = 0;
+            while(number>0)
+            {
+                reminder = number % 10;
+                count++;
+                number = number / 10;
+            }
+            Console.WriteLine("total digits are :" +count);
+            Console.ReadLine();
+        }
+    }
+}
